1b389f1dfa3ade4c2e2ab71aceccdb4e872f6f6c
Dennis Bergevin <dennisbergevin@Denniss-Laptop.localdomain>
2025-10-21 12:30:37 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+<h2 align=center>Cypress plugin filter-runnables</h2>
+<p align="center">
+</p>
+
+<p align="center">
+Cypress plugin that allows user to filter suites/tests in Cypress Test Runner.
+</p>
+
+![cypress-plugin-filter-runnables-demo-1](https://github.com/user-attachments/assets/fc9f3a8a-5de2-4ca1-8b73-2e26bacb3bf6)
+
+
+## Features
+
+- 🔍 A new UI search bar within `cypress open` to filter suites/tests in a given spec
+
+#### Table of Contents
+
+- [Installation](#-installation)
+- [Open mode](#-open-mode)
+  - [Pairing with cypress-plugin-grep-boxes](#pairing-with-cypress-plugin-grep-boxes)
+- [Contributions](#contributions)
+
+---
+
+## 📦 Installation
+
+1. Install the following packages:
+
+```sh
+npm install --save-dev cypress-plugin-filter-runnables
+```
+
+2. In `cypress/support/e2e.js` (For E2E tests) and/or `cypress/support/component.js` (For Component tests),
+
+```js
+import 'cypress-plugin-filter-runnables';
+```
+
+---
+
+## 🔍 Open mode
+
+Run `npx cypress open` to launch Cypress in `open` mode.
+
+Choose a spec file to run and you will be able to filter by suite/test title at the top of the reporter.
+
+### Pairing with `cypress-plugin-grep-boxes`
+
+The [cypress-plugin-grep-boxes](https://github.com/dennisbergevin/cypress-plugin-grep-boxes) is a great companion for this plugin.
+
+Paired with `cypress-plugin-grep-boxes` (`@bahmutov/cy-grep` is also required), you can filter suites/tests and use the checkboxes to run only selected tests:
+
+![cypress-plugin-filter-runnables-demo-2](https://github.com/user-attachments/assets/59053e87-cbfa-4a49-81a0-6212abc94ba8)
+
+Additionally, you can filter tags when paired with `cypress-plugin-grep-boxes` (version `2.1.0` of `@bahmutov/cy-grep` required):
+
+![filter-runnables-grep-boxes-tags](https://github.com/user-attachments/assets/2c359ddf-cf54-4f48-a34a-b9cae4620acd)
+
+In addition, `cypress-plugin-grep-boxes` supports a [disableInitialAutoRun feature](https://github.com/dennisbergevin/cypress-plugin-grep-boxes/blob/main/README.md#disableinitialautorun) that will prevent Cypress Test Runner from automatically running available tests once a spec file is open.
+
+This gives you a moment to filter down to whichever tests to select before running them:
+
+![cypress-plugin-filter-runnables-demo-3](https://github.com/user-attachments/assets/e10262db-7f88-4852-9cc8-1b4626e13f45)
+
+## Contributions
+
+Feel free to open a pull request or drop any feature request or bug in the [issues](https://github.com/dennisbergevin/cypress-plugin-filter-runnables/issues).
+
+Please see more details in the [contributing doc](./CONTRIBUTING.md).
---
+++ index.js
@@ -0,0 +1,160 @@
+/**
+ * Adds a searchbar to reporter to grep suites and tests.
+ */
+
+if (
+  window.top?.document.querySelectorAll('#test-suite-filter-search').length ===
+  0
+) {
+  const style = document.createElement('style');
+  style.textContent = `
+  #test-suite-filter-search::-webkit-search-decoration,
+  #test-suite-filter-search::-webkit-search-cancel-button,
+  #test-suite-filter-search::-webkit-search-results-button,
+  #test-suite-filter-search::-webkit-search-results-decoration {
+    display: none;
+  }
+  #clear-test-suite-filter-search {
+  background: inherit;
+  color: #d0d2e0 !important;
+}
+  #test-suite-filter-search {
+  background: inherit
+}
+`;
+
+  const specContainer = window.top?.document.querySelector('.spec-container');
+  const runnableHeader = window.top?.document.querySelector('.runnable-header');
+
+  window.top?.document.head.appendChild(style);
+  const parentEl = Cypress.version >= '15.0.0' ? specContainer : runnableHeader;
+
+  parentEl.insertAdjacentHTML(
+    'beforebegin',
+    `
+<div class="border-b border-gray-800 h-[64px] mx-[16px] auto-cols-max grid grid-flow-col gap-[8px] grid-cols-[minmax(0,1fr)] pointer-cursor items-center">
+<div class="relative flex items-center w-full h-[48px]">
+
+  <!-- 🔍 Search Icon (Left) -->
+  <div class="absolute left-2 top-1/2 -translate-y-1/2 flex items-center justify-center pointer-events-none">
+    <svg
+      id="search-icon-test-suite-filter"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="icon-dark-gray-800 icon-light-gray-1000"
+      style="min-width: 16px; min-height: 16px;">
+      <path
+        d="M12 7C12 8.38071 11.4404 9.63071 10.5355 10.5355C9.63071 11.4404 8.38071 12 7 12C4.23858 12 2 9.76142 2 7C2 4.23858 4.23858 2 7 2C9.76142 2 12 4.23858 12 7Z"
+        class="icon-light"
+        fill="none" />
+      <path
+        d="M10.5355 10.5355L14 14M10.5355 10.5355C11.4404 9.63071 12 8.38071 12 7C12 4.23858 9.76142 2 7 2C4.23858 2 2 4.23858 2 7C2 9.76142 4.23858 12 7 12C8.38071 12 9.63071 11.4404 10.5355 10.5355Z"
+        class="icon-dark"
+        stroke="#1B1E2E"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round" />
+    </svg>
+  </div>
+
+  <!-- 🏷 Label (Visually Hidden for a11y) -->
+  <label
+    for="test-suite-filter-search"
+    class="cursor-text font-light bottom-[4px] left-[24px] text-gray-500 pointer-events-none absolute sr-only">
+    Filter suites/tests
+  </label>
+
+  <!-- 🔤 Input -->
+  <input
+    id="test-suite-filter-search"
+    type="search"
+    aria-label="filter search for suites and tests"
+    class="pl-8 pr-8 py-3 w-full font-bold text-lg outline-none bg-gray-1100 border-0 placeholder-gray-700 text-white-1500 rounded-md"
+    placeholder="Filter suites/tests"
+    autocapitalize="off"
+    autocomplete="off"
+    spellcheck="false"
+  />
+
+  <!-- ❌ Clear Button (Right) -->
+  <button
+    type="button"
+    id="clear-test-suite-filter-search"
+    aria-label="Clear search field"
+    class="border-transparent rounded-md flex outline-none h-[30px] my-[4px] inset-y-0 right-0 w-[24px] duration-300 absolute items-center justify-center group hocus-default hocus:ring-0">
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      class="icon-light-gray-1000 group-hocus:icon-dark-indigo-300 icon-dark-gray-800"
+      style="min-width: 16px; min-height: 16px;">
+      <path
+        d="M3 13L13 3M3 3L13 13"
+        class="icon-dark"
+        stroke="#1B1E2E"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round" />
+    </svg>
+  </button>
+
+</div>
+</div>  `
+  );
+}
+
+const searchInput = window.top?.document.querySelector(
+  '#test-suite-filter-search'
+);
+const clearBtn = window.top?.document.querySelector(
+  '#clear-test-suite-filter-search'
+);
+
+searchInput?.addEventListener('focus', function () {
+  window.top?.document
+    .querySelector('#search-icon-test-suite-filter')
+    .setAttribute('class', 'icon-dark-indigo-300 icon-light-grey-1700');
+});
+
+searchInput?.addEventListener('focusout', function () {
+  window.top?.document
+    .querySelector('#search-icon-test-suite-filter')
+    .setAttribute('class', 'icon-dark-gray-800 icon-light-gray-1000');
+});
+
+clearBtn?.addEventListener('click', function () {
+  searchInput.value = '';
+  searchInput.focus();
+  searchInput.dispatchEvent(new Event('input', { bubbles: true }));
+});
+
+searchInput?.addEventListener('input', (event) => {
+  const searchTerm = event.target.value.toLowerCase();
+  const testsAndSuites = window.top?.document.querySelectorAll(
+    '.test.runnable, .suite.runnable'
+  );
+
+  for (let i = 0; i < testsAndSuites.length; i++) {
+    const itemText = testsAndSuites[i].textContent.toLowerCase();
+
+    if (itemText.includes(searchTerm)) {
+      testsAndSuites[i].style.display = '';
+    } else {
+      const closestSuiteText = testsAndSuites[i]
+        .closest('.suite.runnable')
+        .innerText.split('\n')[0]
+        .toLowerCase();
+      if (closestSuiteText.includes(searchTerm)) {
+        testsAndSuites[i].style.display = '';
+      } else {
+        testsAndSuites[i].click;
+        testsAndSuites[i].style.display = 'none';
+      }
+    }
+  }
+});
